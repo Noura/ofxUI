5db976fbbccd3744d4e362374f8d72f4c66694e7
noura.howell@gmail.com <noura.howell@gmail.com>
2013-09-16 17:43:10 -0400

    moved cpp content into headers to be consistent with the rest of ofxUI

--- src/ofxUIDraggableRect.h
+++ src/ofxUIDraggableRect.h
@@ -17,25 +17,94 @@ class ofxUIDraggableRect : public ofxUIRectangle {
 
 public:
     
-    ofxUIDraggableRect(float _x, float _y, float _w, float _h, ofRectangle _bounds);
-    ~ofxUIDraggableRect();
+    ofxUIDraggableRect(float _x, float _y, float _w, float _h, ofRectangle _bounds)
+    : ofxUIRectangle(_x, _y, _w, _h)
+    , hit(false)
+    , mouseDx(0)
+    , mouseDy(0)
+    , imagePath("") {
+        bounds = new ofxUIRectangle(_bounds.x, _bounds.y, _bounds.width, _bounds.height);
+        ofAddListener(ofEvents().mousePressed, this, &ofxUIDraggableRect::onMousePressed);
+        ofAddListener(ofEvents().mouseReleased, this, &ofxUIDraggableRect::onMouseReleased);
+        ofAddListener(ofEvents().mouseDragged, this, &ofxUIDraggableRect::onMouseDragged);
+    }
     
-    bool isHit();
-    float getCenterX();
-    float getCenterY();
+    ~ofxUIDraggableRect() {
+        delete bounds;
+        ofRemoveListener(ofEvents().mousePressed, this, &ofxUIDraggableRect::onMousePressed);
+        ofRemoveListener(ofEvents().mouseReleased, this, &ofxUIDraggableRect::onMouseReleased);
+        ofRemoveListener(ofEvents().mouseDragged, this, &ofxUIDraggableRect::onMouseDragged);
+    }
     
-    void setHeight(float _h);
-    void setImage(string _imagePath);
+    bool isHit() {
+        return hit;
+    }
     
-    void draw();
+    float getCenterX() {
+        return getMinX() + width/2;
+    }
     
-    virtual void onMousePressed(ofMouseEventArgs &e);
-    virtual void onMouseReleased(ofMouseEventArgs &e);
-    virtual void onMouseDragged(ofMouseEventArgs &e);
+    float getCenterY() {
+        return getMinY() + height/2;
+    }
+    
+    void setHeight(float _h) {
+        ofxUIRectangle::setHeight(_h);
+        if (imagePath != "") {
+            image.resize(width, height);
+        }
+    }
+    
+    void setImage(string _imagePath) {
+        imagePath = _imagePath;
+        image.loadImage(imagePath);
+        image.resize(width, height);
+    }
+    
+    void draw() {
+        ofxUIRectangle::draw();
+        if (imagePath != "") {
+            ofPushStyle();
+            ofSetColor(255);
+            image.draw(x, y);
+            ofPopStyle();
+        }
+    }
+    
+    virtual void onMousePressed(ofMouseEventArgs &e) {
+        if (inside(e.x, e.y)) {
+            hit = true;
+            mouseDx = e.x - x;
+            mouseDy = e.y - y;
+        }
+    }
+    
+    virtual void onMouseReleased(ofMouseEventArgs &e) {
+        hit = false;
+        mouseDx = 0;
+        mouseDy = 0;
+    }
+    
+    virtual void onMouseDragged(ofMouseEventArgs &e) {
+        //TODO this assumes that this->x and this->y are the top left corner and that bounds->x and bounds->y are the top left corner (i.e. that width and height are both positive for both this and bounds)
+        if (hit) {
+            x = bounded(e.x - mouseDx, bounds->x, bounds->x + bounds->width - width);
+            y = bounded(e.y - mouseDy, bounds->y, bounds->y + bounds->height - height);
+        }
+    }
     
 protected:
     
-    float bounded(float x, float min, float max);
+    float bounded(float x, float min, float max) {
+        if ( x >= min and x <= max) {
+            return x;
+        } else if (x < min) {
+            return min;
+        } else if (x > max) {
+            return max;
+        }
+        return x;
+    }
     
     bool hit;
     float mouseDx, mouseDy;
--- src/ofxUIScrollbarCanvas.h
+++ src/ofxUIScrollbarCanvas.h
@@ -20,22 +20,81 @@ class ofxUIScrollbarCanvas : public ofxUIScrollableCanvas {
 
 public:
     
-    ofxUIScrollbarCanvas(float _x, float _y, float _w, float _h, float _contentHeight = 1000);
-    ~ofxUIScrollbarCanvas();
-    
+    ofxUIScrollbarCanvas(float _x, float _y, float _w, float _h, float _contentHeight = 1000)
+    : ofxUIScrollableCanvas(_x, _y, _w, _h)
+    , init_x(_x) ,init_y(_y), init_w(_w), init_h(_h) {
+        scrollbar_w = OFX_UI_MIN_SCROLLBAR_W;
+        scrollbar_h = OFX_UI_MIN_SCROLLBAR_H;
+        kind = OFX_UI_WIDGET_SCROLLBARCANVAS;
+        
+        scrollbarTrack = new ofRectangle(_x + _w - scrollbar_w, _y, scrollbar_w, _h);
+        scrollbar = new ofxUIDraggableRect(_x + _w - scrollbar_w, _y, scrollbar_w, scrollbar_h, *scrollbarTrack);
+        
+        setContentHeight(_contentHeight);
+    }
+        
+    ~ofxUIScrollbarCanvas() {
+        delete scrollbar;
+        //TODO delete other things (in separate commit)
+    }
+        
     // returns the bounds of the area that is not obscured behind the scroll bar
-    ofRectangle getAvailableSpace();
-    void update();
-    void draw();
+    ofRectangle getAvailableSpace()  {
+        return ofRectangle(rect->x, rect->y, rect->width - scrollbar_w, rect->height);
+    }
+    
+    void draw() {
+        ofxUIScrollableCanvas::draw();
+        scrollbar->draw();
+    }
     
     // You have to tell it how long the content will be if you want it to properly scroll to the bottom of the content.
-    void setContentHeight(float _contentHeight);
+    void setContentHeight(float _contentHeight) {
+        contentHeight = _contentHeight;
+        scrollbar_h = MAX(OFX_UI_MIN_SCROLLBAR_H, MIN(init_h * init_h / contentHeight, init_h));
+        
+        scrollbar->setHeight(scrollbar_h);
+        
+        scrollTop = init_y + scrollbar->height/2;
+        scrollBottom = init_y + init_h - scrollbar->height/2;
+    }
     
-    void setScrollbarImage(string imagePath);
+    // optionally use an image as the scrollbar
+    void setScrollbarImage(string imagePath) {
+        scrollbar->setImage(imagePath);
+    }
     
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button) {
+        if (!sRect->inside(x, y)) {
+            return;
+        }
+        if (!scrollbarTrack->inside(x, y)) {
+            ofxUIScrollableCanvas::mousePressed(x, y, button);
+        }
+    }
+
+    void mouseReleased(int x, int y, int button) {
+        if (!sRect->inside(x, y)) {
+            return;
+        }
+        if (!scrollbarTrack->inside(x,y)) {
+            ofxUIScrollableCanvas::mouseReleased(x, y, button);
+        }
+    }
+
+    void mouseDragged(int x, int y, int button) {
+        if (scrollbar->isHit() && scrollBottom != scrollTop) {
+            float scrollPercent = (scrollbar->getCenterY() - scrollTop) / (scrollBottom - scrollTop);
+            rect->y = init_y - scrollPercent * (contentHeight - init_h);
+        } else {
+            ofxUIScrollableCanvas::mouseDragged(x, y, button);
+            float scrollPercent = (init_y - rect->y) / (contentHeight - init_h);
+            scrollPercent = MAX(0.0, scrollPercent);
+            scrollPercent = MIN(1.0, scrollPercent);
+            float scrollbarY = init_y + scrollPercent * (init_h - scrollbar->height);
+            scrollbar->y = scrollbarY;
+        }
+    }
     
 protected:
     //TODO I think I can use sRect dimensions instead of init_*
@@ -45,8 +104,9 @@ protected:
     float scrollTop, scrollBottom;
     ofxUIDraggableRect * scrollbar;
     ofRectangle * scrollbarTrack;
-    
 };
+
+
 
 
 #endif
